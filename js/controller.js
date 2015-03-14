ca8f6474ca2d2b866a4ff385379baf0b2a8aa259
Marine Boudeau <m14868@gmail.com>
2015-03-14 19:06:34 -0400

    some changes.

--- js/controller.js
+++ js/controller.js
@@ -3,34 +3,13 @@ var app = angular.module('feelioApp', ['ngRoute']);
 function feelioRouteConfig($routeProvider, $locationProvider) {
   $routeProvider.
   when ('/', {
-    templateUrl: '/views/home.html'
+    templateUrl: '/views/popular.html'
   }).
-  when ('/about', {
-    templateUrl: '/views/about.html'
+  when ('/popular', {
+    templateUrl: '/views/popular.html'
   }).
   when ('/contact', {
     templateUrl: '/views/contact.html'
-  }).
-  when ('/nyc', {
-    templateUrl: '/views/home-nyc.html'
-  }).
-  when ('/sf', {
-    templateUrl: '/views/home-sf.html'
-  }).
-  when ('/paris', {
-    templateUrl: '/views/home-paris.html'
-  }).
-  when ('/signup', {
-    templateUrl: '/views/signup.html'
-  }).
-  when ('/almost', {
-    templateUrl: '/views/almost.html'
-  }).
-  when ('/welcome', {
-    templateUrl: '/views/welcome.html'
-  }).
-  when ('/goodbye', {
-    templateUrl: '/views/goodbye.html'
   }).
   otherwise ({
     redirectTo: '/'
