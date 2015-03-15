5273b034d4d33bee04a1ff4f52de72ea011a0774
Marine Boudeau <m14868@gmail.com>
2015-03-14 23:26:35 -0400

    test

--- js/controller.js
+++ js/controller.js
@@ -24,7 +24,7 @@ app.controller("PostsCtrl", function($scope, $http) {
     success(function(data) {
       //$scope.posts = data;
       //$scope.posts = data.feed.entry[0]['gsx$title']['$t'];
-      $scope.posts = data.feed.entry[0];
+      $scope.posts = data.feed.entry;
       // $scope.random = function() {
       //   return 0.5 - Math.random();
       // }
@@ -33,6 +33,58 @@ app.controller("PostsCtrl", function($scope, $http) {
       //console.log("not able to pull data");
     });
 });
+
+
+
+app.controller("anotherCtrl", function($scope, $http){
+    $scope.results = [];
+    $scope.filterText = null;
+    $scope.availableCategories = [];
+    $scope.categoryFilter = null;
+
+    $scope.init = function() {
+    $http.jsonp('https://spreadsheets.google.com/feeds/list/1lZWwacSVxTD_ciOsuNsrzeMTNAl0Dj8SOrbaMqPKM7U/oh9bt7w/public/values?alt=json-in-script' + '&callback=JSON_CALLBACK').success(function(data) {
+      angular.forEach(data, function(value, index){
+        angular.forEach(value.entry, function(classes, index){
+          $scope.results.push(classes);
+            angular.forEach(classes.gsx$category, function(category, index){
+              var exists = false;
+                angular.forEach($scope.availableCategories, function(avCat, index){
+                  if (avCat == category) {
+                    exists = true;
+                  }
+                });
+                if (exists === false) {
+                  $scope.availableCategories.push(category);
+                }
+              });
+            });
+          });
+        }).error(function(error) {
+      });
+    };
+
+    $scope.setCategoryFilter = function(category) {
+    $scope.categoryFilter = category;
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.controller('MainCtrl', function($scope, $route, $routeParams, $location) {
   $scope.$route = $route;
