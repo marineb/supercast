9d609316fd268f49228584ac9c059b7a8976d42d
Marine Boudeau <m14868@gmail.com>
2015-03-14 22:22:19 -0400

    test spreadsheet

--- js/controller.js
+++ js/controller.js
@@ -24,7 +24,8 @@ app.controller("PostsCtrl", function($scope, $http) {
   $http.get('https://spreadsheets.google.com/feeds/list/1lZWwacSVxTD_ciOsuNsrzeMTNAl0Dj8SOrbaMqPKM7U/oh9bt7w/public/values?alt=json-in-script&callback=x').
     success(function(data) {
       //$scope.posts = data;
-      $scope.posts = data.feed.entry[0]['gsx$title']['$t'];
+      //$scope.posts = data.feed.entry[0]['gsx$title']['$t'];
+      $scope.posts = data.feed.entry;
       $scope.random = function() {
         return 0.5 - Math.random();
       }
