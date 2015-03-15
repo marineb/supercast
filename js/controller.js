553432e7a0752321b8ccafb2949967fe7d574e2f
Marine Boudeau <m14868@gmail.com>
2015-03-14 22:58:36 -0400

    test

--- js/controller.js
+++ js/controller.js
@@ -20,7 +20,6 @@ function feelioRouteConfig($routeProvider, $locationProvider) {
 
 
 app.controller("PostsCtrl", function($scope, $http) {
-  //$http.get('http://jsonplaceholder.typicode.com/posts').
   $http.get('https://spreadsheets.google.com/feeds/list/1lZWwacSVxTD_ciOsuNsrzeMTNAl0Dj8SOrbaMqPKM7U/oh9bt7w/public/values?alt=json-in-script&callback=x').
     success(function(data) {
       //$scope.posts = data;
@@ -34,17 +33,6 @@ app.controller("PostsCtrl", function($scope, $http) {
       console.log("not able to pull data");
     });
 });
-
-
-// $http({method: 'GET', url: 'json/json_price_1.json'}).success(function(data) {
-//     $scope.artists = [];
-//     angular.forEach(data.artists, function(value, key) {
-//         $scope.artists.push(value);
-//     });
-//     $scope.isVisible = function(name){
-//         return true;// return false to hide this artist's albums
-//     };
-// });
 
 app.controller('MainCtrl', function($scope, $route, $routeParams, $location) {
   $scope.$route = $route;
