356f0d7c148dfe82983c7ad6968b06e930351cbc
Marine Boudeau <m14868@gmail.com>
2015-07-12 05:30:01 -0400

    Updated GA code

--- js/ga.js
+++ js/ga.js
@@ -3,6 +3,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48128728-1', 'feelio.cc');
+ga('create', 'UA-23108818-2', 'auto');
 ga('require', 'displayfeatures');
 //ga('send', 'pageview'); // comment out for angular.
